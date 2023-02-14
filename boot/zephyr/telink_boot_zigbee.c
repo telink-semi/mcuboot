e7548e608bf809fa6546b11850e98aa6e3af93b4
Andy Liu <jie.liu@telink-semi.com>
2023-02-14 15:43:22 +0800

    UTCSH-188: restore all IRQ priorities to reset values

--- boot/zephyr/telink_boot_zigbee.c
+++ boot/zephyr/telink_boot_zigbee.c
@@ -4,13 +4,30 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include <stdint.h>
-#include "bootutil/bootutil_log.h"
-
+#include <zephyr/sw_isr_table.h>
+#include <bootutil/bootutil_log.h>
 #include <flash.h>
 
 BOOT_LOG_MODULE_DECLARE(mcuboot);
 
+#define PLIC_PRIO (0xe4000000)
+#define PLIC_IRQS (CONFIG_NUM_IRQS - CONFIG_2ND_LVL_ISR_TBL_OFFSET)
+
 #define ZIGBEE_IMAGE_START_ADDRESS (0x20000000 + 0x10000)
+
+static void restore_all_irq_priorities(void)
+{
+    volatile uint32_t *prio = (volatile uint32_t *)PLIC_PRIO;
+    int i;
+
+    /* Set priority of each interrupt line back to 1 */
+    /* Interrupt source 0 does not exist*/
+    for (i = 1; i < PLIC_IRQS; i++)
+    {
+        *prio = 1U;
+        prio++;
+    }
+}
 
 void boot_zigbee_image(void)
 {
@@ -26,6 +43,9 @@ void boot_zigbee_image(void)
     BOOT_LOG_INF("2nd byte = 0x%x", *(byte + 1));
     BOOT_LOG_INF("3rd byte = 0x%x", *(byte + 2));
     BOOT_LOG_INF("4th byte = 0x%x", *(byte + 3));
+
+    BOOT_LOG_INF("restore_all_irq_priorities()");
+    restore_all_irq_priorities();
 
     BOOT_LOG_INF("booting...");
     /* Jump to entry point of the Zigbee image*/
